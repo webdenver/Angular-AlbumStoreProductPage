a110b9ef36ff1a82d6a885048de07406d5247083
webdenver <webdenver@github.com>
2020-03-08 19:22:19 +0500

    Displaying a List of Products (6)

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,13 +3,19 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
+  private _productsUrl: string = '../assets/products.json';
+
   constructor(private _http: Http) { }
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map( response => <Album>response.json());
+  }
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map( response => <Product[]>response.json());
   }
 }
